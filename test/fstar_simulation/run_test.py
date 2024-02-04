159596a3ce2fd4094ae097aafdd61faf9081c066
Michele Bianco <mbianco@protonmail.com>
2024-02-04 21:05:45 +0100

    debug 4

--- test/fstar_simulation/run_test.py
+++ test/fstar_simulation/run_test.py
@@ -15,7 +15,7 @@ paramfile = sys.argv[1]             # Name of the parameter file
 sim = pc2r.C2Ray_fstar(paramfile=paramfile, Nmesh=256, use_gpu=True, use_mpi=False)
 
 # Get redshift list (test case)
-idx_zred, zred_array = np.loadtxt(sim.inputs_basename+'redshift_checkpoints.txt', dtype=float)
+idx_zred, zred_array = np.loadtxt(sim.inputs_basename+'redshift_checkpoints.txt', dtype=float, unpack=True)
 
 # check for resume simulation
 if(sim.resume):
