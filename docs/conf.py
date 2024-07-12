463f74a18374564d4df052f356c2fbfc16b0c91a
Michele Bianco <mbianco@protonmail.com>
2024-07-12 21:38:35 +0200

    added extension for readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -9,3 +9,19 @@ import sphinx_rtd_theme
 html_theme = "sphinx_rtd_theme"
 html_logo = 'fig/logo.png'
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.coverage",
+    "sphinx.ext.doctest",
+    "sphinx.ext.extlinks",
+    "sphinx.ext.ifconfig",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.todo",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.autosectionlabel",
+    "numpydoc",
+    "nbsphinx"
+]
