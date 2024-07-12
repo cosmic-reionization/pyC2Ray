d4beef44ae4c4f3828b48eec010ae446382fafc9
Michele Bianco <mbianco@protonmail.com>
2024-07-12 17:57:37 +0200

    more error from readthedocs build

--- docs/conf.py
+++ docs/conf.py
@@ -1,5 +1,5 @@
-import sphinx_rtd_theme
+import sphinx_readable_theme
 
 # Add these lines
 html_theme = "readable"
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme_path = [sphinx_readable_theme.get_html_theme_path()]
